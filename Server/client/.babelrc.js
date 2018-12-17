f9bd6936ea063f4d8114ef5cff02aa7a4894dc80
Xinyudanielzhao <xinyu@uwm.edu>
2018-12-17 15:18:10 -0600

    last step

--- Server/client/.babelrc.js
+++ Server/client/.babelrc.js
@@ -1,6 +1,5 @@
 module.exports = api => {
   api.cache.using(() => {
-    // cache based on the two env vars
     return 'babel:' + process.env.BABEL_TARGET +
       ' protractor:' + process.env.IN_PROTRACTOR;
   });
